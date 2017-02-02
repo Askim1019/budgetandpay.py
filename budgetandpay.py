4dcd5f56c0ff3c8d6443548f1e199354ec7733c7
Alex Kim <askim1019@gmail.com>
2017-02-02 00:52:44 -0600

    Add files via upload

---
+++ budgetandpay.py
@@ -0,0 +1,63 @@
+#Make a new program to compute someone's pay for a week, a month, a year
+#Be sure to include tax rate as well
+#This will not include medical insurance deductions from the pay
+print("This program calculates pay for time worked and monthly expenses. Overtime is considered anything over 40 hours")
+print()
+inp = input("Name: ")
+try:
+    inp = input("Enter hours worked: ")
+    hours = float(inp)
+    inp = input("Enter rate/hour: $")
+    rate = float(inp)
+    inp = input("tax(%): ")
+    tax = float(inp)/100
+except:
+    print("Error! Please enter numeric values")
+    
+if hours > 40:
+    grosspay = rate * 40 + (1.5 * rate * (hours - 40))
+else:
+    grosspay = rate * hours
+
+netpay_week = grosspay - (tax * grosspay)
+print()
+print("grosspay for the week is $", grosspay)
+print("netpay for the week is $", netpay_week)
+
+netpay_month = netpay_week * 4
+print("netpay for the month is $", netpay_month)
+
+netpay_year = netpay_month * 12
+print("netpay for the year is $", netpay_year)
+print()
+
+x = 5
+while x == 5:
+    inp1 = input('Enter expense name(if there are no expenses type "no more" and press Enter: ')
+    if inp1 == 'no more':
+        print("Total yearly expense is $0")
+        break
+    else:
+        inp = input("Enter minimum monthly payment: $")
+        expmonth = float(inp)
+        print()
+        expyear = expmonth * 12
+        print("Total yearly expense of ", inp1, " is $", expyear)
+        print()
+        print("Total yearly expense of ", inp1, " is $", expmonth)
+        print()
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
